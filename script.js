d5fb2cd87cef4962753ba9f9e042595f92cfb192
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-10-28 01:59:05 +0900

    wip

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function R_Click(p_janken_r, player_id) {
     let janken_r = Math.floor(Math.random() * 3);  //Math.random() で乱数を作ります
     let p_janken = ['グー', 'チョキ', 'パー'];  //"プレイヤーのjanken"のリストを作成します。
 
+    if (janken_r == 0) {
+        comPatarnChart.data.datasets[0].data[0]++;
+        comPatarnChart.update();
+    } else if (janken_r == 1) {
+        comPatarnChart.data.datasets[0].data[1]++;
+        comPatarnChart.update();
+    } else if (janken_r == 2) {
+        comPatarnChart.data.datasets[0].data[2]++;
+        comPatarnChart.update();
+    }
+
     //勝ち負けの判定機プログラムです
     if (janken_r === p_janken_r) {
         Result_end = '<span class="font-bold text-orange-600 bg-orange-200">&nbsp;&nbsp;あいこです&nbsp;&nbsp;</span>';
@@ -31,8 +42,12 @@ function R_Click(p_janken_r, player_id) {
     document.getElementById("Rejan1_" + player_id).innerHTML = "「" + p_janken[p_janken_r] + "」&nbsp;を選択！ジャンケンの結果は …？";
     document.getElementById("Rejan2_" + player_id).innerHTML = "相手は 「" + janken[janken_r] + "」。よって " + Result_end;
     document.getElementById("todays-grades")
-        .innerHTML = "全&nbsp;" + (total_win + total_drow + total_lose) + "&nbsp;試合&nbsp;" + total_win + "勝&nbsp;" + total_lose + "敗&nbsp;" + total_drow + "分&nbsp;<br>連勝記録：" + total_winning_streak_todays_max + "連勝！";
+        .innerHTML = "全&nbsp;" + (total_win + total_drow + total_lose) + "&nbsp;試合&nbsp;" + total_win + "勝&nbsp;" + total_lose + "敗&nbsp;" + total_drow + "分&nbsp;<br>連勝記録：" + total_winning_streak_todays_max + " 連勝！";
     document.getElementById("todays-win-rate").innerHTML = "勝率" + (Math.floor((total_win / (total_win + total_drow + total_lose)) * 100)) + "%";
+
+    myChart.data.labels.push(total_win + total_drow + total_lose);
+    myChart.data.datasets[0].data.push(Math.floor((total_win / (total_win + total_drow + total_lose)) * 100));
+    myChart.update();
 }
 
 function win_act() {
@@ -57,52 +72,51 @@ function winning_streak_refresh() {
 
 
 
+// chart.js --------------------------------------------------------------------------------------------------------------
 
-
-
-function R_Click_2(p_janken_r) {
-    let janken = ['グー', 'チョキ', 'パー',];//"janken"のリストを作成します。
-    let janken_r = Math.floor(Math.random() * 3);//Math.random() で乱数を作ります
-
-    let p_janken = ['グー', 'チョキ', 'パー'];//"プレイヤーのjanken"のリストを作成します。
-    //勝ち負けの判定機プログラムです
-    if (janken_r === p_janken_r) {
-        Result_end = "あいこです";
-    } else if (p_janken_r === 0 && janken_r === 1) {
-        Result_end = "あなたの【勝ち】";
-    } else if (p_janken_r === 1 && janken_r === 2) {
-        Result_end = "あなたの【勝ち】";
-    } else if (p_janken_r === 2 && janken_r === 0) {
-        Result_end = "あなたの【勝ち】";
-    } else {
-        Result_end = "あなたの【負け】";
+// chart.js
+var ctx = document.getElementById('canvas').getContext('2d');
+window.myChart = new Chart(ctx, { // インスタンスをグローバル変数で生成
+    type: 'bar',
+    data: { // ラベルとデータセット
+        datasets: [{
+            label: [],
+            data: [], // グラフデータ
+            backgroundColor: 'rgb(0, 134, 197, 0.7)', // 棒の塗りつぶし色
+            borderColor: 'rgba(0, 134, 197, 1)', // 棒の枠線の色
+            borderWidth: 1, // 枠線の太さ
+            max: 100,
+        }],
+    },
+    options: {
+        legend: {
+            display: false, // 凡例を非表示
+        }
     }
+});
 
-    //結果を表示するためのプログラムです
-    document.getElementById("Rejan1_2").innerHTML = p_janken[p_janken_r] + "を選択しました。ジャンケンの結果は・・・？";
-    document.getElementById("Rejan2_2").innerHTML = "相手は" + janken[janken_r] + Result_end;
-}
-
-
-function R_Click_3(p_janken_r) {
-    let janken = ['グー', 'チョキ', 'パー',];//"janken"のリストを作成します。
-    let janken_r = Math.floor(Math.random() * 3);//Math.random() で乱数を作ります
-
-    let p_janken = ['グー', 'チョキ', 'パー'];//"プレイヤーのjanken"のリストを作成します。
-    //勝ち負けの判定機プログラムです
-    if (janken_r === p_janken_r) {
-        Result_end = "あいこです";
-    } else if (p_janken_r === 0 && janken_r === 1) {
-        Result_end = "あなたの【勝ち】";
-    } else if (p_janken_r === 1 && janken_r === 2) {
-        Result_end = "あなたの【勝ち】";
-    } else if (p_janken_r === 2 && janken_r === 0) {
-        Result_end = "あなたの【勝ち】";
-    } else {
-        Result_end = "あなたの【負け】";
+var comPatarnChart = new Chart(document.getElementById("comPatarnChart"), {
+    type: 'doughnut',
+    data: {
+        labels: ['グー', 'チョキ', 'パー'],
+        datasets: [{
+            label: '# of Tomatoes',
+            data: [0, 0, 0],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(255, 206, 86, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        //cutoutPercentage: 40,
+        responsive: false,
     }
-
-    //結果を表示するためのプログラムです
-    document.getElementById("Rejan1_3").innerHTML = p_janken[p_janken_r] + "を選択しました。ジャンケンの結果は・・・？";
-    document.getElementById("Rejan2_3").innerHTML = "相手は" + janken[janken_r] + Result_end;
-}
+});
